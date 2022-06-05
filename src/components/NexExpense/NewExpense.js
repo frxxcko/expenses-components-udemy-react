c81c6e31544ce474c8132e8caf188eda27f5e488
Juan <72244366+frxxcko@users.noreply.github.com>
2022-06-05 20:02:04 -0300

    Add NewExpense form componentes to create new expenses

---
+++ src/components/NexExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExpenseForm from '../ExpenseForm/ExpenseForm';
+
+const NewExpense = () => {
+    return <>
+        <ExpenseForm />
+    </>
+}
+
+export default NewExpense;

ab10a6f3ac90ebd9003b4a10cba49dd1e73fb777
Juan Basualdo <basualdo.ij@gmail.com>
2022-06-09 00:29:12 -0300

    Lifting the State up to parent component - from ExpensesFilter to Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter'
 
-function Expenses(props){
-    return(
+const Expenses = (props) => {
+    const [selectedYear, setSelectedYear] = useState('Choose a year');
+    
+    const getSelectedYear = (selectedYear) => {
+        // set State with year picked in ExpensesFilter child component
+        setSelectedYear(selectedYear)
+    }
+
+    return (
         <>
-            {props.expenses.map( expense => <ExpenseItem expense={expense} key={`expense_${expense.id}`}/>)}
+            <ExpensesFilter onSelectYear={getSelectedYear} yearSelected={selectedYear} />
+            {props.expenses.map(expense => <ExpenseItem expense={expense} key={`expense_${expense.id}`} />)}
         </>
     )
 }
